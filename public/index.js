6dee542a2ce47b0c8c20b9696ef31c7c153b2487
Mark Hernandez <mh81796@gmail.com>
2017-03-14 22:09:05 -0500

    Finish computer prototype

--- public/index.js
+++ public/index.js
@@ -1,13 +1,17 @@
-/* global $ */
+/* global $ location */
 
 $('#next').on('click', function () {
   if ($('.current').next().length > 0) {
     $('.current').removeClass('current').addClass('hide').next().removeClass('hide').addClass('current')
+    page++
+    $('#location').text(page + ' / 6')
   }
 })
 
 $('#prev').on('click', function () {
   if ($('.current').prev().length > 0) {
     $('.current').removeClass('current').addClass('hide').prev().removeClass('hide').addClass('current')
+    page--
+    $('#location').text(page + ' / 6')
   }
 })
